41371c88a64874d89db9ab783b291445218582c5
albinmartin <albwe681@gmail.com>
2014-03-05 19:29:51 +0100

    added useful help classes

---
+++ monostrategy/Utility/Grid3D.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace test3d
+{
+    public class Grid3D<T> 
+    {
+        private T[, ,] tiles;
+        private int gridSize;
+        protected float tileSize;
+
+        protected int GridSize1
+        {
+            get { return gridSize; }
+            set { gridSize = value; }
+        }
+
+        public T[, ,] Tiles
+        {
+            get { return tiles; }
+            set { tiles = value; }
+        }
+
+        public int GridSize
+        {
+            get { return gridSize; }
+            set { gridSize = value; }
+        }
+
+        public Grid3D(int gridSize, float tileSize)
+        {
+            tiles = new T[gridSize, gridSize, gridSize];
+            this.gridSize = gridSize;
+            this.tileSize = tileSize;
+        }
+        
+        protected int toGridCoord(float fp)
+        {
+            return (int)((fp + tileSize / 2) / tileSize);
+        }
+
+        protected Vector3 toGridCoord(Vector3 fp)
+        {
+            return new Vector3(toGridCoord(fp.X), toGridCoord(fp.Y), toGridCoord(fp.Z));
+        }
+
+        protected bool inGrid(int ip)
+        {
+            return ip >= 0 && ip < GridSize;
+      
+        }
+
+        private bool inGrid(int x, int y, int z)
+        {
+            return inGrid(x) && inGrid(y) && inGrid(z);
+
+        }
+
+        public bool IsPointInGrid(Vector3 point)
+        {
+            int x = toGridCoord(point.X);
+            int y = toGridCoord(point.Y);
+            int z = toGridCoord(point.Z);
+            return (inGrid(x) && inGrid(y) && inGrid(z));
+        }
+
+        public T SelectCube(Vector3 position)
+        {
+            int x = toGridCoord(position.X);
+            int y = toGridCoord(position.Y);
+            int z = toGridCoord(position.Z);
+            
+            return Tiles[x, y, z];
+        }
+
+        public List<T> SelectCubesInLine(Vector3 l1, Vector3 l2, int width)
+        {
+            List<Vector3> coordinates = new List<Vector3>();
+            return SelectCubesInLine(l1, l2, width, ref coordinates);
+        }
+
+        public List<T> SelectCubesInLine(Vector3 l1, Vector3 l2, int width,  ref List<Vector3> coordinates)
+        {
+            List<T> result = new List<T>();
+            coordinates = new List<Vector3>(); //debug
+            int x1 = toGridCoord(l1.X);
+            int y1 = toGridCoord(l1.Y);
+            int z1 = toGridCoord(l1.Z);
+
+            int x2 = toGridCoord(l2.X);
+            int y2 = toGridCoord(l2.Y);
+            int z2 = toGridCoord(l2.Z);
+
+            int dx = x2 - x1;
+            int dy = y2 - y1;
+            int dz = z2 - z1;
+
+            int ax = Math.Abs(dx) * 2;
+            int ay = Math.Abs(dy) * 2;
+            int az = Math.Abs(dz) * 2;
+
+            int sx = Math.Sign(dx);
+            int sy = Math.Sign(dy);
+            int sz = Math.Sign(dz);
+
+            int x = x1;
+            int y = y1;
+            int z = z1;
+
+            if (ax >= Math.Max(ay, az)) // x dominant
+            {
+                float yd = ay - ax / 2;
+                float zd = az - ax / 2;
+
+                while (true)
+                {
+                    for (int zw = -width; zw <= width; zw++)
+                    {
+                        for (int yw = -width; yw <= width; yw++)
+                        {
+                            if (inGrid(x, y + yw, z + zw) && Tiles[x, y + yw, z + zw] != null)
+                                result.Add(Tiles[x, y + yw , z + zw]);
+                            coordinates.Add(new Vector3(x, y + yw, z + zw));
+                        }
+                    }
+
+                    if (x == x2)
+                        break;
+                    if (yd >= 0)
+                    {
+                        y = y + sy;
+                        yd = yd - ax;
+                    }
+                    if (zd >= 0)
+                    {
+                        z = z + sz;
+                        zd = zd - ax;
+                    }
+                    x = x + sx;
+                    yd = yd + ay;
+                    zd = zd + az;
+                }
+            }
+            else if (ay >= Math.Max(ax, az)) //y dominant
+            {
+                float xd = ax - ay / 2;
+                float zd = az - ay / 2;
+
+                while (true)
+                {
+                    for (int zw = -width; zw <= width; zw++)
+                    {
+                        for (int xw = -width; xw <= width; xw++)
+                        {
+                            if (inGrid(x + xw, y, z + zw) && Tiles[x + xw, y, z + zw] != null)
+                                result.Add(Tiles[x + xw, y, z + zw]);
+                            coordinates.Add(new Vector3(x + xw, y, z + zw));
+                        }
+                    }
+                    if (y == y2)
+                        break;
+                    if (xd >= 0)
+                    {
+                        x = x + sx;
+                        xd = xd - ay;
+                    }
+                    if (zd >= 0)
+                    {
+                        z = z + sz;
+                        zd = zd - ax;
+                    }
+                    y = y + sy;
+                    xd = xd + ax;
+                    zd = zd + az;
+                }
+            }
+            else if (az >= Math.Max(ax, ay)) //z dominant
+            {
+                float xd = ax - az / 2;
+                float yd = ay - az / 2;
+
+                while (true)
+                {
+                    for (int yw = -width; yw <= width; yw++)
+                    {
+                        for (int xw = -width; xw <= width; xw++)
+                        {
+                            if (inGrid(x + xw, y + yw, z) && Tiles[x + xw, y + yw, z] != null)
+                                result.Add(Tiles[x + xw, y + yw, z]);
+                            coordinates.Add(new Vector3(x + xw, y + yw, z));
+                        }
+                    }
+                    if (z == z2)
+                        break;
+                    if (xd >= 0)
+                    {
+                        x = x + sx;
+                        xd = xd - az; 
+                    }
+                    if (yd >= 0)
+                    {
+                        y = y + sy;
+                        yd = yd - az;
+                    }
+
+                    z = z + sz; 
+                    xd = xd + ax;
+                    yd = yd + ay;
+                }
+            }
+            return result;
+        }
+
+        public List<T> SelectCubesInBox(Vector3 position, Vector3 bounds)
+        {
+            List<T> result = new List<T>();
+            for (int x = toGridCoord(position.X) - toGridCoord(bounds.X / 2); x <= toGridCoord(position.X) + toGridCoord(bounds.X / 2); x++)
+            {
+                for (int y = toGridCoord(position.Y) - toGridCoord(bounds.Y / 2); y <= toGridCoord(position.Y) + toGridCoord(bounds.Y / 2); y++)
+                {
+                    for (int z = toGridCoord(position.Z) - toGridCoord(bounds.Z / 2); z <= toGridCoord(position.Z) + toGridCoord(bounds.Z / 2); z++)
+                    {
+                        if (inGrid(x) && inGrid(y) && inGrid(z) && Tiles[x, y, z] != null)
+                            result.Add(Tiles[x, y, z]);
+                    }
+                }
+            }
+            return result;
+        }
+
+        public List<T> SelectCubesOnSphere(Vector3 position, float radius, int longs, int lats, ref List<Vector3> coordinates)
+        {
+            List<T> result = new List<T>();
+            coordinates = new List<Vector3>();
+            Vector3 gPos = toGridCoord(position);
+            int i, j;
+            for (i = 0; i <= lats; i++)
+            {
+                float lat0 = (float)Math.PI * (-0.5f + (float)(i - 1) / lats);
+                float z0 = (float)Math.Sin(lat0) * radius;
+                float zr0 = (float)Math.Cos(lat0) * radius;
+
+                float lat1 = (float)Math.PI * (-0.5f + (float)i / lats);
+                float z1 = (float)Math.Sin(lat1) * radius;
+                float zr1 = (float)Math.Cos(lat1) * radius;
+
+                for (j = 0; j <= longs; j++)
+                {
+                    float lng = 2 * (float)Math.PI * (float)(j - 1) / longs;
+                    float x = (float)Math.Cos(lng);
+                    float y = (float)Math.Sin(lng);
+                    coordinates.Add(this.toGridCoord(new Vector3(x * zr0, y * zr0, z0)) + gPos);
+                    coordinates.Add(this.toGridCoord(new Vector3(x * zr1, y * zr1, z1)) + gPos);
+                    //plot(x * zr0, y * zr0, z0);
+                    //plot(x * zr1, y * zr1, z1);
+                }
+            }
+
+            return result;
+        }
+
+
+        public List<T> SelectCubesInBox(Vector3 position, Vector3 bounds, ref List<Vector3> coordinates)
+        {
+            List<T> result = new List<T>();
+            coordinates = new List<Vector3>();
+            for (int x = toGridCoord(position.X) - toGridCoord(bounds.X / 2); x <= toGridCoord(position.X) + toGridCoord(bounds.X / 2); x++)
+            {
+                for (int y = toGridCoord(position.Y) - toGridCoord(bounds.Y / 2); y <= toGridCoord(position.Y) + toGridCoord(bounds.Y / 2); y++)
+                {
+                    for (int z = toGridCoord(position.Z) - toGridCoord(bounds.Z / 2); z <= toGridCoord(position.Z) + toGridCoord(bounds.Z / 2); z++)
+                    {
+                        if (inGrid(x) && inGrid(y) && inGrid(z) && Tiles[x, y, z] != null)
+                            result.Add(Tiles[x, y, z]);
+                        coordinates.Add(new Vector3(x, y, z));
+                    }
+                }
+            }
+            return result;
+        }
+
+    }
+
+}
---
+++ monostrategy/Utility/MathHelperC.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace monostrategy.Utility
+{
+    class MathHelperC
+    {
+        public static float PI = (float)Math.PI;
+        private static Random rand = new Random(2);
+
+        public static Random Rand
+        {
+            get { return MathHelperC.rand; }
+            set { MathHelperC.rand = value; }
+        }
+
+        public static float RandomFloat()
+        {
+            return (float)rand.NextDouble() - 0.5f;
+        }
+
+        public static float RandomFloat(Random rand)
+        {
+            return (float)rand.NextDouble() - 0.5f;
+        }
+
+        public static float Pow(float x, float y)
+        {
+            return (float)Math.Pow((double)x, (double)y);
+        }
+
+        public static float GetAngle(Vector2 v)
+        {
+            return (float)Math.Atan2((float)v.Y, (float)v.X);
+        }
+
+        public static Vector2 RandomVector()
+        {
+            Vector2 v = new Vector2(RandomFloat(), RandomFloat());
+            v.Normalize();
+
+            return v;
+        }
+
+        public static Vector2 GetVector2(float radians)
+        {
+            return new Vector2((float)Math.Cos(radians), (float)Math.Sin(radians));
+        }
+
+        public static float ToRadians(float degrees)
+        {
+            return degrees * PI / 180;
+        }
+
+        public static float ToDegrees(float radians)
+        {
+            return radians * 180 / PI;
+        }
+
+        public static Vector2 RotateAroundPoint(Vector2 point, Vector2 origin, float radians)
+        {
+            float rotation = radians;
+            Vector2 u = point - origin;
+
+            if (u == Vector2.Zero)
+                return point;
+
+            float a = (float)Math.Atan2(u.Y, u.X);
+            a += rotation;
+            u = new Vector2((float)Math.Cos(a), (float)Math.Sin(a)) * u.Length();
+            return u + origin;
+        }
+
+        public static List<Vector2> LinePositions(Vector2 start, Vector2 stop)
+        {
+            List<Vector2> coordinates = new List<Vector2>();
+            int width = 0;
+            int x1 = (int)((start.X + 0) / 100);
+            int y1 = (int)((start.Y + 0) / 100);
+
+            int x2 = (int)((stop.X + 0) / 100);
+            int y2 = (int)((stop.Y + 0) / 100);
+
+
+            int dx = x2 - x1;
+            int dy = y2 - y1;
+
+
+            int ax = Math.Abs(dx) * 2;
+            int ay = Math.Abs(dy) * 2;
+
+            int sx = Math.Sign(dx);
+            int sy = Math.Sign(dy);
+
+
+            int x = x1;
+            int y = y1;
+
+            if (ax >= ay) // x dominant
+            {
+                float yd = ay - ax / 2;
+
+                while (true)
+                {
+                    for (int yw = -width; yw <= width; yw++)
+                        coordinates.Add(new Vector2(x, y + yw) * 100);
+
+                    if (x == x2)
+                        break;
+                    if (yd >= 0)
+                    {
+                        y = y + sy;
+                        yd = yd - ax;
+
+                        for (int yw = -width; yw <= width; yw++)
+                            coordinates.Add(new Vector2(x, y + yw) * 100);
+                    }
+
+                    x = x + sx;
+                    yd = yd + ay;
+                }
+            }
+            else if (ay >= ax) //y dominant
+            {
+                float xd = ax - ay / 2;
+
+                while (true)
+                {
+                    for (int xw = -width; xw <= width; xw++)
+                        coordinates.Add(new Vector2(x + xw, y) * 100);
+                    if (y == y2)
+                        break;
+                    if (xd >= 0)
+                    {
+                        x = x + sx;
+                        xd = xd - ay;
+
+                        for (int xw = -width; xw <= width; xw++)
+                            coordinates.Add(new Vector2(x + xw, y) * 100);
+                    }
+                    y = y + sy;
+                    xd = xd + ax;
+                }
+            }
+
+            return coordinates;
+        }
+
+        #region Intersections
+        public static bool IntersectsCircleCircle(Vector2 p1, float r1, Vector2 p2, float r2)
+        {
+            return (p1 - p2).Length() < r1 + r2;
+        }
+
+        private static bool FindLineIntersection(Vector2 start1, Vector2 end1, Vector2 start2, Vector2 end2, out Vector2 poi)
+        {
+            poi = new Vector2(float.PositiveInfinity, float.PositiveInfinity);
+            float denom = ((end1.X - start1.X) * (end2.Y - start2.Y)) - ((end1.Y - start1.Y) * (end2.X - start2.X));
+
+            //  AB & CD are parallel 
+            if (denom == 0)
+                return false;
+
+            float numer = ((start1.Y - start2.Y) * (end2.X - start2.X)) - ((start1.X - start2.X) * (end2.Y - start2.Y));
+
+            float r = numer / denom;
+
+            float numer2 = ((start1.Y - start2.Y) * (end1.X - start1.X)) - ((start1.X - start2.X) * (end1.Y - start1.Y));
+
+            float s = numer2 / denom;
+
+            if ((r < 0 || r > 1) || (s < 0 || s > 1))
+                return false;
+
+            // Find intersection point
+            poi = new Vector2();
+            poi.X = start1.X + (r * (end1.X - start1.X));
+            poi.Y = start1.Y + (r * (end1.Y - start1.Y));
+
+            return true;
+        }
+
+        public static bool IntesectsLineCircle(Vector2 circleOrigin, float radius, Vector2 lineStart, Vector2 lineEnd)
+        {
+            Vector2 dir = lineEnd - lineStart;
+            Vector2 diff = circleOrigin - lineStart;
+            float t = Vector2.Dot(diff, dir) / Vector2.Dot(dir, dir);
+            if (t < 0.0f)
+                t = 0.0f;
+            if (t > 1.0f)
+                t = 1.0f;
+            Vector2 closest = lineStart + t * dir;
+            Vector2 d = circleOrigin - closest;
+            float distsqr = Vector2.Dot(d, d);
+            return distsqr <= radius * radius;
+
+
+
+            /*Vector2 newOrigin = circleOrigin - lineStart;
+            Vector2 vtRay = lineEnd - lineStart;
+            if (vtRay == Vector2.Zero)
+                return false;
+            vtRay.Normalize();
+            Vector2 pointOfIntersection = Vector2.Zero;
+            float t = Vector2.Dot(newOrigin, vtRay) / Vector2.Dot(vtRay, vtRay);
+            if (t < 0)
+                return false;
+
+            float b = newOrigin.X * vtRay.X + vtRay.Y * newOrigin.Y;
+            float c = radius * radius - (newOrigin.X * newOrigin.X + newOrigin.Y * newOrigin.Y);
+            float a = vtRay.X * vtRay.X + vtRay.Y * vtRay.Y;
+
+            float diva = 1.0f / a;
+
+            if((b*b + a*c) <= 0)
+                return false;
+
+            float b4ac = (float)Math.Sqrt((b * b + a * c));
+            float l1 = (b - b4ac) * diva;
+            float l2 = (b + b4ac) * diva;
+
+            float l;
+            if (l1 < l2)
+                l = l1;
+            else
+                l = l2;
+
+            pointOfIntersection = (vtRay * l) + lineStart;
+            return true;*/
+        }
+
+        public static bool IntersectsCircleRectangle(Vector2 circleOrigin, float radius, Vector2 rectangleOrigin, float width, float height)
+        {
+            float circleDistanceX = Math.Abs(circleOrigin.X - rectangleOrigin.X - width / 2);
+            float circleDistanceY = Math.Abs(circleOrigin.Y - rectangleOrigin.Y - height / 2);
+
+            if (circleDistanceX > (width / 2 + radius)) { return false; }
+            if (circleDistanceY > (height / 2 + radius)) { return false; }
+
+            if (circleDistanceX <= (width / 2)) { return true; }
+            if (circleDistanceY <= (height / 2)) { return true; }
+
+            float cornerDistance_sq = (float)(Math.Pow((circleDistanceX - width / 2), 2) +
+                                 Math.Pow((circleDistanceY - height / 2), 2));
+
+            return (cornerDistance_sq <= (Math.Pow(radius, 2)));
+        }
+
+        public static float GetShortestAngle(float target, float current)
+        {
+            float angle = (target - current);
+            while (angle > PI)
+                angle -= 2 * PI;
+            while (angle < -PI)
+                angle += 2 * PI;
+
+            return angle;
+        }
+        #endregion
+    }
+
+}
